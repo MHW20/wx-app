48d83f1f573ce548417c659248c8bceae221bbea
MHW20 <matthewhanna70@gmail.com>
2024-08-27 16:53:56 +0100

    Updated directory structure for features and added css for basic layout of website

---
+++ wx-frontend/src/pages/home.tsx
@@ -0,0 +1,23 @@
+import './home.css'
+
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="leftPanel">
+          <div id='searchBoxContainer'>
+            
+          </div>
+          <div id='recentLocationsContainer'>
+
+          </div>
+        </div>
+        <div className="rightPanel">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
