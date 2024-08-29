c0880d575d1eb3130e717a513415d9f42e4c83da
MHW20 <matthewhanna70@gmail.com>
2024-08-29 15:47:17 +0100

    Added validation to only trigger the useQuery hooks when the parameters are defined

--- wx-frontend/src/features/Search/hooks/SearchHooks.ts
+++ wx-frontend/src/features/Search/hooks/SearchHooks.ts
@@ -2,9 +2,17 @@ import { useQuery } from "react-query";
 import { fetchForecastByLatLon, fetchLocationByName } from "../services/searchService";
 
 export const useLocation = (input: string) => {
-  return useQuery(["location", input], () => { fetchLocationByName(input)})
+  return useQuery({
+    queryKey: ["location", input],
+    enabled: !!input,
+    queryFn: () => fetchLocationByName(input)
+  })
 }
 
 export const useForecast = (lat: number, lon: number) => {
-  return useQuery(["forecast", lat, lon], () => { fetchForecastByLatLon(lat, lon)})
+  return useQuery({
+    queryKey: ["location", lat, lon],
+    enabled: typeof lat === 'number' && typeof lon === 'number',
+    queryFn:() => fetchForecastByLatLon(lat, lon)
+  })
 }
